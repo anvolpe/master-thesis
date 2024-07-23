94550c2f17cdbd6713b302e9b63234ffd2e662ef
Alina <st177132@stud.uni-stuttgart.de>
2024-07-23 20:38:44 +0200

    added dual annealing bounds tests
    single_optimizer_experiment_dual_annealing test bfgs, cobyla and several bounds for dual annealing
    results in ../results/dual_annealing_results/ for first 47 configurations

--- Code/entangled_qnn_training-main/project_qnn_experiments_main.py
+++ Code/entangled_qnn_training-main/project_qnn_experiments_main.py
@@ -27,7 +27,7 @@ tols = [1e-5, 1e-10]
 #tols = [1e-10, 1e-15] schlechte ergebnisse, 1e-5 viel besser
 #tols = [1e-2, 1e-5]
 #bounds = [(0,2*np.pi)*dimensions]
-bounds = list(zip(np.zeros(6), np.ones(6)*2*np.pi))
+default_bounds = list(zip(np.zeros(6), np.ones(6)*2*np.pi))
 #bounds = list(zip(np.ones(6)*(-2)*np.pi, np.ones(6)*2*np.pi))
 learning_rates = [0.01, 0.001, 0.0001]
 
@@ -147,7 +147,7 @@ def sgd_experiment(objective,initial_param_values,opt):
     return results
 
 # gibt nicht direkt sowas wie ftol und eps
-def dual_annealing_experiment(objective,initial_param_values):
+def dual_annealing_experiment(objective,initial_param_values,bounds):
     results = {"type": "gradient-free"} 
     run_n = 0
     for max_iter in max_iters:
@@ -195,7 +195,6 @@ def single_config_experiments(conf_id, data_type, num_data_points, s_rank, unita
     # return ist jeweils result-dict (das auch alle Spezifikationen enthält?)
     # das in der Datei conf_[conf_id]_opt.csv speichern
 
-        
 def single_optimizer_experiment(conf_id, databatch_id, data_type, num_data_points, s_rank, unitary, data_points):
     '''
     Run all optimizer experiments for a single config & databatch combination
@@ -251,6 +250,80 @@ def single_optimizer_experiment(conf_id, databatch_id, data_type, num_data_point
                 opt_name = variant.__name__
                 #result_dict[opt_name] = future.result()
                 result_dict[opt_name] = result
+        else:
+            #future = exe.submit(opt, objective, initial_param_values)
+            result = opt(objective,initial_param_values)
+            opt_name = opt.__name__.removesuffix('_experiment')
+            #result_dict[opt_name] = future.result()
+            result_dict[opt_name] = result
+
+    return result_dict
+        
+def single_optimizer_experiment_dual_annealing(conf_id, databatch_id, data_type, num_data_points, s_rank, unitary, data_points):
+    '''
+    Run all optimizer experiments for a single config & databatch combination
+
+    Return:
+        dict containing all specifications of optimizers & results
+    '''
+    result_dict = {}
+    data_points_string = (
+        np.array2string(data_points.numpy(), separator=",")
+        .replace("\n", "")
+        .replace(" ", "")
+    )
+    result_dict["databatch"] = data_points_string
+    initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions)
+    initial_param_values_string = (
+        np.array2string(initial_param_values, separator=",")
+        .replace("\n", "")
+        .replace(" ", "")
+    )
+    result_dict["initial param values"] = initial_param_values_string
+    
+    # specifications of qnn
+    qnn = CudaPennylane(num_wires=num_qubits, num_layers=num_layers, device="cpu") 
+    
+    expected_output = torch.matmul(unitary, data_points)
+    y_true = expected_output.conj()
+    
+    # objective function based on cost function of qnn 
+    def objective(x):
+        qnn.params = torch.tensor(x, dtype=torch.float64, requires_grad=True).reshape(qnn.params.shape) # stimmt das???????
+        cost = cost_func(data_points, y_true, qnn, device="cpu") 
+        return cost.item()
+
+    # verschiedene inital_param_values ausprobieren und avg bilden? 
+    #initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions) # [0,2pi] siehe victor_thesis_landscapes.py, bei allen optimierern gleich
+    #initial_param_values_tensor = torch.tensor(initial_param_values)
+
+    # run optimizer experiments
+    sgd_optimizers = [sgd, rmsprop, adam]
+    #sgd_optimizers = [adam]
+    optimizers = [bfgs_experiment, cobyla_experiment, dual_annealing_experiment]
+    bound_batches = []
+    bounds = list(zip(np.zeros(6), np.ones(6)*2*np.pi))
+    bound_batches.append(bounds)
+    bound_batches.append(list(zip(np.zeros(6), np.ones(6)*3*np.pi)))
+    bound_batches.append(list(zip(np.zeros(6), np.ones(6)*4*np.pi)))
+    bound_batches.append(list(zip(np.ones(6)*(-2)*np.pi, np.ones(6)*2*np.pi)))
+    bound_batches.append(list(zip(np.ones(6)*(-3)*np.pi, np.ones(6)*3*np.pi)))
+    bound_batches.append(list(zip(np.ones(6)*(-4)*np.pi, np.ones(6)*4*np.pi)))
+    # TODO: ProcessPoolExecutor: funktioniert nicht, weil pickle verwendet wird und objective eine lokal definierte Funktion ist 
+    # (AttributeError: Can't pickle local object 'test_experiment.<locals>.objective')
+    # Multiprocessing (??) könnte funktionieren. Oder eigene Klasse??
+    # with ProcessPoolExecutor(cpu_count()) as exe:
+    for opt in optimizers:
+        if opt == dual_annealing_experiment:
+            i=0
+            for bounds in bound_batches:
+                #future = exe.submit(sgd_experiment, objective, initial_param_values_tensor, variant)
+                result = dual_annealing_experiment(objective,initial_param_values,bounds)
+                opt_name = f'dual_annealing_{i}'
+                #result_dict[opt_name] = future.result()
+                result["bounds"] = bounds
+                result_dict[opt_name] = result
+                i += 1
         else:
             #future = exe.submit(opt, objective, initial_param_values)
             result = opt(objective,initial_param_values)
@@ -470,7 +543,72 @@ def test_single_optimizer():
     res = minimize(objective, initial_param_values, method=adam, 
                         options={"maxiter": 50, "learning_rate":0.001, "eps":1e-5})
 
- 
+def test_dual_annealing():
+    '''
+    Read all configurations of qnn and databatches from configurations_16_6_4_10_13_3_14.txt and run optimizer experiments
+    for every configuration & databatch combination
+    Creates json file for every configuration that saves all specifications for configuration and optimizer results.
+    File is saved as "experimental_results/results/optimizer_results/conf_[conf_id]_opt.json"
+    '''
+    filename = "Code/entangled_qnn_training-main/experimental_results/configs/configurations_16_6_4_10_13_3_14.txt"
+    file = open(filename, 'r')
+    Lines = file.readlines()
+    n = 0
+    conf_id = 0
+    databatch_id = 0
+    data_type = ""
+    num_data_points = 0
+    s_rank = 0
+    unitary = []
+    databatches = []
+    result_dict = {}
+
+    for line in Lines:
+        if(line.strip() == "---"): # config has been fully read, run optimizer experiments for each data_point-tensor (5)
+            # setup dictionary for dumping info into json file later
+            date = datetime.now()
+            result_dict = {"date": date.strftime("%Y/%m/%d/, %H:%M:%S"), "conf_id":conf_id, "data_type":data_type, "num_data_points":num_data_points, "s_rank":s_rank}
+            unitary_string = (
+                np.array2string(unitary.numpy(), separator=",")
+                .replace("\n", "")
+                .replace(" ", "")
+            )
+            result_dict["unitary"] = unitary_string
+            n = 0
+            #databatch = databatches[n]
+            #print(len(databatches))
+            start = time.time()
+            for i in range(len(databatches)): 
+                data_points = databatches[i]
+                dict = single_optimizer_experiment_dual_annealing(conf_id, i, data_type, num_data_points, s_rank, unitary, data_points)
+                databatch_key = f"databatch_{databatch_id}"
+                result_dict[databatch_key] = dict
+                #print(conf_id, data_type, num_data_points, s_rank)
+            #write results to json file
+            duration = np.round((time.time()-start),2)
+            print(f"config {conf_id}: {duration/60}min")
+            result_dict["duration (s)"] = duration
+            os.makedirs("experimental_results/results/optimizer_results/dual_annealing", exist_ok=True)
+            file = open(f"experimental_results/results/optimizer_results/conf_{conf_id}_dual_annealing.json", mode="w")
+            json.dump(result_dict, file)
+            databatches = []
+            unitary = []
+            n += 1
+
+        else:
+            var, val = line.split("=")
+            if(var == "conf_id"): conf_id = int(val) 
+            elif(var == "data_type"): data_type = val # random, orthogonal, non_lin_ind, var_s_rank
+            elif(var == "num_data_points"): num_data_points = int(val) 
+            elif(var == "s_rank"): s_rank = int(s_rank) # Schmidt-Rank
+            elif(var == "unitary"): 
+                val,_ = re.subn('\[|\]|\\n', '', val) 
+                unitary = torch.from_numpy(np.fromstring(val,dtype=complex,sep=',').reshape(-1,4))#unitary: 4x4 tensor
+            elif(var.startswith("data_batch_")): 
+                val,_ = re.subn('\[|\]|\\n', '', val)
+                #print(torch.from_numpy(np.fromstring(val,dtype=complex,sep=',').reshape(-1,4)))
+                databatches.append(torch.from_numpy(np.fromstring(val,dtype=complex,sep=',').reshape(-1,4,4))) #data_points: 1x4x4 tensor
+
 
 if __name__ == "__main__":
     #single_optimizer_experiment(1, "random",1,1,[],[])
@@ -482,7 +620,7 @@ if __name__ == "__main__":
     
     start = time.time()
     print(f"start time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start))}")
-    run_all_optimizer_experiments()
+    test_dual_annealing()
     #test_several_optimizers()
     #test_single_optimizer()
     print(f"total runtime: {np.round((time.time()-start)/60,2)}min") 
