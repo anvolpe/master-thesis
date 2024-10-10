99c954d2b6a3d6cd2d287863d3c866cd05d0a143
Alina <st177132@stud.uni-stuttgart.de>
2024-10-10 15:51:33 +0200

    new stuff

--- Code/entangled_qnn_training-main/project_qnn_experiments_main.py
+++ Code/entangled_qnn_training-main/project_qnn_experiments_main.py
@@ -1,6 +1,5 @@
-import csv
-import gc
 import json
+import multiprocessing
 import time
 from datetime import datetime
 from qnns.cuda_qnn import CudaPennylane
@@ -10,17 +9,15 @@ from victor_thesis_landscapes import *
 from victor_thesis_plots import *
 from victor_thesis_metrics import *
 from victor_thesis_experiments_main import *
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from multiprocessing import cpu_count
 from sgd_for_scipy import *
-from jax import jacrev
 import os
-import pandas as pd
 from scipy.optimize import minimize, dual_annealing
 import re
 
-no_of_runs = 1
-#no_of_runs = 10
+#no_of_runs = 1
+no_of_runs = 10
 
 num_layers = 1
 num_qubits = 2
@@ -244,69 +241,81 @@ def dual_annealing_experiment(objective,initial_param_values,bounds=default_boun
     return results
 
 
-def single_optimizer_experiment(conf_id, databatch_id, data_type, num_data_points, s_rank, unitary, data_points):
+def single_optimizer_experiment(conf_id, run_id, data_type, num_data_points, s_rank, unitary, databatches):
     '''
     Run all optimizer experiments for a single config & databatch combination
 
     Return:
         dict containing all specifications of optimizers & results
     '''
+    start = time.time()
     result_dict = {}
-    data_points_string = (
-        np.array2string(data_points.numpy(), separator=",")
-        .replace("\n", "")
-        .replace(" ", "")
-    )
-    result_dict["databatch"] = data_points_string
-    initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions)
-    initial_param_values_string = (
-        np.array2string(initial_param_values, separator=",")
-        .replace("\n", "")
-        .replace(" ", "")
-    )
-    result_dict["initial param values"] = initial_param_values_string
-    
-    # specifications of qnn
-    qnn = CudaPennylane(num_wires=num_qubits, num_layers=num_layers, device="cpu") 
-    
-    expected_output = torch.matmul(unitary, data_points)
-    y_true = expected_output.conj()
-    
-    # objective function based on cost function of qnn 
-    def objective(x):
-        qnn.params = torch.tensor(x, dtype=torch.float64, requires_grad=True).reshape(qnn.params.shape) # stimmt das???????
-        cost = cost_func(data_points, y_true, qnn, device="cpu") 
-        return cost.item()
+    for i in range(len(databatches)):
+        data_points = databatches[i]
+        databatch_key = f"databatch_{i}"
+        result_dict[databatch_key] = {}
+        data_points_string = (
+            np.array2string(data_points.numpy(), separator=",")
+            .replace("\n", "")
+            .replace(" ", "")
+        )
+        result_dict[databatch_key]["databatch"] = data_points_string
+        initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions)
+        initial_param_values_string = (
+            np.array2string(initial_param_values, separator=",")
+            .replace("\n", "")
+            .replace(" ", "")
+        )
+        result_dict[databatch_key]["initial param values"] = initial_param_values_string
+        
+        # specifications of qnn
+        qnn = CudaPennylane(num_wires=num_qubits, num_layers=num_layers, device="cpu") 
+        
+        expected_output = torch.matmul(unitary, data_points)
+        y_true = expected_output.conj()
+        
+        # objective function based on cost function of qnn 
+        def objective(x):
+            qnn.params = torch.tensor(x, dtype=torch.float64, requires_grad=True).reshape(qnn.params.shape) # stimmt das???????
+            cost = cost_func(data_points, y_true, qnn, device="cpu") 
+            return cost.item()
 
-    # verschiedene inital_param_values ausprobieren und avg bilden? 
-    #initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions) # [0,2pi] siehe victor_thesis_landscapes.py, bei allen optimierern gleich
-    #initial_param_values_tensor = torch.tensor(initial_param_values)
+        # verschiedene inital_param_values ausprobieren und avg bilden? 
+        #initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions) # [0,2pi] siehe victor_thesis_landscapes.py, bei allen optimierern gleich
+        #initial_param_values_tensor = torch.tensor(initial_param_values)
 
-    # run optimizer experiments
-    sgd_optimizers = [sgd, rmsprop, adam]
-    #sgd_optimizers = [adam]
-    optimizers = [nelder_mead_experiment, bfgs_experiment, cobyla_experiment, powell_experiment, slsqp_experiment, sgd_experiment, dual_annealing_experiment]
-    
-    # TODO: ProcessPoolExecutor: funktioniert nicht, weil pickle verwendet wird und objective eine lokal definierte Funktion ist 
-    # (AttributeError: Can't pickle local object 'test_experiment.<locals>.objective')
-    # Multiprocessing (??) könnte funktionieren. Oder eigene Klasse??
-    # with ProcessPoolExecutor(cpu_count()) as exe:
-    for opt in optimizers:
-        if opt == sgd_experiment:
-            for variant in sgd_optimizers:
-                #future = exe.submit(sgd_experiment, objective, initial_param_values_tensor, variant)
-                result = sgd_experiment(objective,initial_param_values,variant)
-                opt_name = variant.__name__
+        # run optimizer experiments
+        sgd_optimizers = [sgd, rmsprop, adam]
+        #sgd_optimizers = [adam]
+        optimizers = [nelder_mead_experiment, bfgs_experiment, cobyla_experiment, powell_experiment, slsqp_experiment, sgd_experiment, dual_annealing_experiment]
+        
+        # TODO: ProcessPoolExecutor: funktioniert nicht, weil pickle verwendet wird und objective eine lokal definierte Funktion ist 
+        # (AttributeError: Can't pickle local object 'test_experiment.<locals>.objective')
+        # Multiprocessing (??) könnte funktionieren. Oder eigene Klasse??
+        #with ProcessPoolExecutor(cpu_count()) as exe:
+        #with multiprocessing.pool.Pool() as pool:
+        for opt in optimizers:
+            if opt == sgd_experiment:
+                for variant in sgd_optimizers:
+                    #future = exe.submit(sgd_experiment, objective, initial_param_values, variant)
+                    result = sgd_experiment(objective,initial_param_values,variant)
+                    #future = pool.map_async(sgd_experiment, (objective, initial_param_values, variant,))
+                    opt_name = variant.__name__
+                    #result_dict[opt_name] = future.get()
+                    #result_dict[opt_name] = future.result()
+                    result_dict[databatch_key][opt_name] = result
+            else:
+                #future = exe.submit(opt, objective, initial_param_values)
+                result = opt(objective,initial_param_values)
+                #future = pool.map_async(opt, (objective, initial_param_values,))
+                opt_name = opt.__name__.removesuffix('_experiment')
+                #result_dict[opt_name] = future.get()
                 #result_dict[opt_name] = future.result()
-                result_dict[opt_name] = result
-        else:
-            #future = exe.submit(opt, objective, initial_param_values)
-            result = opt(objective,initial_param_values)
-            opt_name = opt.__name__.removesuffix('_experiment')
-            #result_dict[opt_name] = future.result()
-            result_dict[opt_name] = result
-
-    return result_dict
+                result_dict[databatch_key][opt_name] = result
+    duration = np.round((time.time()-start),2)
+    print(f"config {conf_id}, run {run_id}: {duration/60}min")
+    result_dict["duration (s)"] = duration
+    return run_id, result_dict
         
 def single_config_experiment_bounds(conf_id, databatch_id, data_type, num_data_points, s_rank, unitary, data_points):
     '''
@@ -410,30 +419,28 @@ def run_all_optimizer_experiments():
         if(line.strip() == "---"): # config has been fully read, run optimizer experiments for each data_point-tensor (5)
             # setup dictionary for dumping info into json file later
             date = datetime.now()
-            result_dict = {"date": date.strftime("%Y/%m/%d/, %H:%M:%S"), "conf_id":conf_id, "data_type":data_type, "num_data_points":num_data_points, "s_rank":s_rank}
+            result_dict_template = {"date": date.strftime("%Y/%m/%d/, %H:%M:%S"), "conf_id":conf_id, "data_type":data_type, "num_data_points":num_data_points, "s_rank":s_rank}
             unitary_string = (
                 np.array2string(unitary.numpy(), separator=",")
                 .replace("\n", "")
                 .replace(" ", "")
             )
-            result_dict["unitary"] = unitary_string
+            result_dict_template["unitary"] = unitary_string
             
             n = 0
-            for run_id in range(no_of_runs):
-                start = time.time()
-                for i in range(len(databatches)): 
-                    data_points = databatches[i]  
-                    dict = single_optimizer_experiment(conf_id, i, data_type, num_data_points, s_rank, unitary, data_points)
-                    databatch_key = f"databatch_{i}"
-                    result_dict[databatch_key] = dict
-                    #print(conf_id, data_type, num_data_points, s_rank)
-                #write results to json file
-                duration = np.round((time.time()-start),2)
-                print(f"config {conf_id}: {duration/60}min")
-                result_dict["duration (s)"] = duration
-                os.makedirs("experimental_results/results/optimizer_results", exist_ok=True)
-                file = open(f"experimental_results/results/optimizer_results/conf_{conf_id}_run_{run_id}_opt.json", mode="w")
-                json.dump(result_dict, file)
+            with ProcessPoolExecutor(max_workers=5) as exe:
+                futures = [exe.submit(single_optimizer_experiment,conf_id, run_id, data_type, num_data_points, s_rank, unitary, databatches) for run_id in range(no_of_runs)]
+                
+                for future in as_completed(futures):
+	                # get the result for the next completed task
+                    run_id, result_dict = future.result()# blocks
+                    # create complete result dictionary (begins with result_dict_template)
+                    dict = result_dict_template
+                    dict.update(result_dict)
+                    #write results to json file
+                    os.makedirs("experimental_results/results/optimizer_results", exist_ok=True)
+                    file = open(f"experimental_results/results/optimizer_results/conf_{conf_id}_run_{run_id}_opt.json", mode="w")
+                    json.dump(dict, file)
             databatches = []
             unitary = []
             n += 1
--- Code/entangled_qnn_training-main/testMain.py
+++ Code/entangled_qnn_training-main/testMain.py
@@ -1,6 +1,4 @@
-import csv
-import gc
-import json
+import multiprocessing
 import time
 from datetime import datetime
 from qnns.cuda_qnn import CudaPennylane
@@ -12,10 +10,11 @@ from victor_thesis_metrics import *
 from victor_thesis_experiments_main import *
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
-import os
-import pandas as pd
-from scipy.optimize import minimize, rosen, rosen_der
-
+from multiprocessing.pool import Pool
+import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
+import re
+'''
 def generate_and_save_testLandscape():
     print("####### Generate and Save test landscape")
     num_layers = 1
@@ -125,14 +124,14 @@ def run_single_optimizer_experiment():
     # input x: is a 1D array with 6 values (since: 6 parameters)
     # output: corresponding loss 
     def objective(x):
-        '''
+        
             objective to minimize based on loss landscape
 
             Args:
                 x (1D array): is a 1D array with 6 values (since: 6 parameters)
             Returns:
                 float: corresponding loss 
-        '''
+        
         obj_value = 0
 
         # TODO: Index mithilfe von x und grid_size berechnen
@@ -204,24 +203,99 @@ def optimizer_experiment():
 def testMinimizeBounds():
     x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
     bounds = list(zip(np.zeros(5), np.ones(6)*2))
-    res = minimize(rosen, x0, method='BFGS', jac=rosen_der, bounds=bounds, options={'gtol': 1e-6, 'disp': True})
+    res = minimize(rosen, x0, method='BFGS', jac=rosen_der, bounds=bounds, options={'gtol': 1e-6, 'disp': True})'''
+
+def func1():
+    time.sleep(3)
+    print("done with func1")
+    return None
+
+def func2():
+    time.sleep(4)
+    print("done with func2")
+    return None
+
+def pso_test():
+    num_layers = 1
+    num_qubits = 2
+    dimensions = 6
+    max_iters = [100,500,1000]
+        # setup qnn configuration
+    conf_id = 0
+    data_type = "random" # random, orthogonal, non_lin_ind, var_s_rank
+    num_data_points = 1
+    s_rank = 1 # Schmidt-Rank
+    var = "data_batch_0"
+    databatch_id = int(var.strip("datbch_"))
+
+    # setup dictionary for dumping info into json file later
+    result_dict = {"conf_id":conf_id, "data_type":data_type, "num_data_points":num_data_points, "s_rank":s_rank}
+    val = "[[-0.45070455-0.02711853j,0.78437395-0.06613086j,0.06358678+0.19963393j,-0.07343613+0.35668523j],[-0.01890143-0.03813363j,0.32408202+0.25557629j,0.05872864-0.68979805j,0.55466693-0.20227297j],[-0.11215405+0.64023111j,-0.13344055+0.29565494j,-0.49012687-0.19046288j,-0.04241254+0.44046348j],[0.55771659+0.24656916j,0.31851997-0.05798805j,0.28761525-0.34294258j,-0.56718418+0.03616933j]]"
+    result_dict["unitary"] = val#.strip("\"")
+    val,_ = re.subn('\[|\]|\\n', '', val)
+    unitary = torch.from_numpy(np.fromstring(val,dtype=complex,sep=',').reshape(-1,4))#unitary: 4x4 tensor
+    val = '[[[0.09314128-0.12946863j,0.39382838-0.19799267j,0.05133879+0.12112185j,0.08106995-0.04021906j],[0.07622026-0.09754417j,0.31152873-0.14143589j,0.03608905+0.09551662j,0.06411194-0.02869752j],[0.11804856-0.19626647j,0.54031288-0.32976236j,0.08774511+0.16729872j,0.1112873-0.06711204j],[-0.01827577+0.10086995j,-0.17383409+0.2237231j,-0.06326177-0.05610261j,-0.03593256+0.04574145j]]]'
+    result_dict[var] = {}
+    result_dict[var]["databatch"] = val#.strip("\"")
+    val,_ = re.subn('\[|\]|\\n', '', val) 
+    data_points = torch.from_numpy(np.fromstring(val,dtype=complex,sep=',').reshape(-1,4,4)) #data_points: 1x4x4 tensor
+
+
+    # setup qnn
+    qnn = CudaPennylane(num_wires=num_qubits, num_layers=num_layers, device="cpu") 
+    expected_output = torch.matmul(unitary, data_points)
+    y_true = expected_output.conj()
+    
+    # objective function based on cost function of qnn 
+    def objective(x):
+        '''
+            x  is of size num_particles x dimensions
+            returns array of length num_particles (cost-value for each particle)
+        '''
+        n_particles = x.shape[0]
+        cost_values = []
+        for i in range(n_particles):
+            qnn.params = torch.tensor(x[i], dtype=torch.float64, requires_grad=True).reshape(qnn.params.shape) # stimmt das???????
+            cost = cost_func(data_points, y_true, qnn, device="cpu") 
+            cost_values.append(cost.item())
+        return cost_values
+    results = {}
+    # verschiedene inital_param_values ausprobieren und avg bilden? 
+    # TODO: gleich für alle Konfigs oder nur gleich für eine Konfig aber da für alle Optmimierer?
+    initial_param_values = np.random.uniform(0, 2*np.pi, size=dimensions) # [0,2pi] siehe victor_thesis_landscapes.py, bei allen optimierern gleich
+    initial_param_values_tensor = torch.tensor(initial_param_values)
+
+    options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+    run_n = 0
+    for max_iter in max_iters:
+        #for tol in tols:
+        #for catol in tols:
+                # Call instance of PSO
+                optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=dimensions, options=options)
+
+                # Perform optimization
+                start = time.time()
+                res, pos = optimizer.optimize(objective, iters=max_iter)
+                duration = time.time() - start
+                # fill results dict
+                # specifications of this optimizer run
+                results[run_n] = {"maxiter": max_iter, "duration":duration}
+                # result info
+                '''for attribute in res.keys():
+                    results[run_n][attribute] = str(res[attribute])'''
+                results[run_n]["fun"] = res
+                results[run_n]["x"] = pos
+                results[run_n]["callback"] = list(optimizer.cost_history) # stimmt das??
+                global nit 
+                nit = 0
+                run_n += 1
+    print(results)
 
 if __name__ == "__main__":
-    #single_test_run()
-    #run_full_experiment()
-    #generate_and_save_testLandscape()
-    #run_single_optimizer_experiment()
-    #testMinimizeBounds()
-    values = [[1,2,3,4,5], [3,4], [0,1,2]]
-    max_len = len(max(values, key=len))
-    print(max_len)
-    # pad right of each sublist of fun_values with optimal fun value to make it as long as the longest sublist
-    for sublist in values:
-        opt_fun_val = sublist[-1]
-        sublist[:] = sublist + [opt_fun_val] * (max_len - len(sublist))
-    print(values)
-    for sublist in values:
-        print(len(sublist))
+    pso_test()
+
+
+
 
 
     
